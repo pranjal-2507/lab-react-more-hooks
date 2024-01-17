3351fb4b621ba3b902fe90ee93a42dee93d6a547
Pranjal <pranjalgosavi2507@gmail.com>
2024-01-17 09:33:17 +0530

    Completed

---
+++ More_Hooks/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Tasklist from './Components/TaskList'
+
+
+
+function App() {
+ 
+  return (
+    <>
+     <Tasklist/>
+    </>
+  )
+}
+
+export default App
